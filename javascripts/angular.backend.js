0d5936df9ecbd6434b26758a92b0a9360cebd846
macecyri <ac@cymace.com>
2013-10-03 20:12:42 -0700

    regular commit

--- javascripts/angular.backend.js
+++ javascripts/angular.backend.js
@@ -33,27 +33,43 @@ var ListClassifiedController = function ($scope, $routeParams) {
     ];
 
     $scope.test = $routeParams.labels;
-    $scope.tags = ['label1', 'label2', 'label3'];
-    $scope.tagsList = ['label1', 'label2', 'label3', 'label4', 'label5', 'label6'];
-    $scope.href = $scope.tags.join('+');
+    $scope.searchTags = ['label1', 'label2', 'label3'];
+    $scope.allTags = [{title:'tag1', status:'addable-tag'}, {title:'tag2', status:'addable-tag'}, {title:'tag3', status:'addable-tag'}, {title:'tag4', status:'addable-tag'}, {title:'tag5', status:'addable-tag'} ];
 
-    $scope.removetag = function (tag) {
-        var index = $scope.tags.indexOf(tag);
+
+    $scope.removeSearchTag = function (tag) {
+        var index = $scope.searchTags.indexOf(tag);
         if (index > -1) {
-            $scope.tags.splice(index, 1);
+            $scope.searchTags.splice(index, 1);
+            updateTagSelection(tag, 'addable-tag');
         }
-        $scope.test = tag;
-        $scope.href = $scope.tags.join('+');
+        updateHrefSearch();
     }
 
-    $scope.addTag = function (tag) {
-        if ($.inArray(tag, $scope.tags) === -1){
-            $scope.tags.push(tag);
-            $scope.test = tag;
-            $scope.href = $scope.tags.join('+');
+
+
+    $scope.addSearchTag = function (tag) {
+        if ($.inArray(tag, $scope.searchTags) === -1){
+            $scope.searchTags.push(tag);
+            updateTagSelection(tag, 'added-tag');
+            updateHrefSearch();
         }
     }
 
+
+    var updateTagSelection = function (tag, status) {
+        for(var i = 0; i < $scope.allTags.length; i += 1) {
+            if($scope.allTags[i].title === tag) {
+                $scope.allTags[i].status = status;
+            }
+        }
+    }
+
+    var updateHrefSearch = function () {
+        $scope.href = $scope.searchTags.join('+');
+    }
+
+    updateHrefSearch();
 
 };
 
